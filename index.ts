eae6e917f1676bf75d263850c7b95fbac0367935
Nicholas Osei <Niconaldodonjunior@yahoo.com>
2021-03-14 00:51:59 +0100

    allcode

--- index.ts
+++ index.ts
@@ -1,9 +1,48 @@
+import * as AWS from 'aws-sdk'
 import{
     APIGatewayProxyHandler,
     APIGatewayEvent,
     APIGatewayProxyResult,
     Context,
   } from 'aws-lambda';
+
+  const BUCKET_NAME = "test2231";
+  const s3 = new AWS.S3();
+
+  const getFiles = async () =>
+  {
+    //   const files = [{name:'fake.jpeg'}];
+    //   return {files};
+
+    const files :Array<{name:string}>=[];
+    const data = await s3.listObjectsV2({Bucket:BUCKET_NAME}).promise();
+    if(data && data.Contents)
+    {
+        for(const item of data.Contents)
+        {
+            files.push({name:item.Key})
+        }
+        
+    }
+    return {files}
+
+  }
+
+  const deleteFiles = async (name:string)=>
+  {
+    await s3.deleteObject({Key:name,
+        Bucket:BUCKET_NAME}).promise();
+    return{};
+  }
+
+  const postFiles = async (name:string, contentType:string,content:string)=>{
+      await s3.upload({Key:name,
+    Bucket:BUCKET_NAME,
+ContentType:contentType,
+Body:content}).promise();
+return{};
+  };
+
   
   const handler: APIGatewayProxyHandler= async(event: APIGatewayEvent, context:Context)=>{
     let body={}
